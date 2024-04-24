78876948f3fa616642c034839f49f99ade6f322e
yangyanbo <yangyanbo@fuzhi.ai>
2024-04-24 17:05:32 +0800

    feat: polyfill

--- vite-hooks/vite.config.ts
+++ vite-hooks/vite.config.ts
@@ -4,8 +4,18 @@ import virtual from './plugins/virtual-module';
 import requireTransform from 'vite-plugin-require-transform';
 import svgr from './plugins/svgr';
 import inspect from 'vite-plugin-inspect';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), virtual(), requireTransform({}), svgr({}), inspect()],
+  plugins: [
+    react(),
+    virtual(),
+    requireTransform({}),
+    svgr({}),
+    inspect(),
+    legacy({
+      targets: ['ie >= 11'],
+    }),
+  ],
 });
